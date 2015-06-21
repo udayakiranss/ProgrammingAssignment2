c16abffb78d372ffe926876f1b319dbefd5f8b68
Udaya Kiran S S <udaykiranss@icloud.com>
2015-06-21 19:50:10 +0530

    Added comments to the R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,43 @@
-## This is a utility which helps in inversing the matrix of type cacheMatrix and caching the inversing matrix. 
-## It has 2 functions
-## i. makeCacheMatrix - Input: Matrix OutPut: List of functions() to get/set matrix along with methods to obtain the cached matrix
-## ii.cacheSolve - Input: Spl Matrix of type cache matrix  Output : Inverse of spl matrix
+## This is an utility R file which helps in creating and caching of a special matrix  and 
+## provides the function to inverse the special matrix and cache the inverse matrix.
+## It uses below utility functions
+## i. makeCacheMatrix
+## ii.cacheSolve
 
 
-## makeCacheMatrix caches the inverse matrix for the first time and subsequently same matrix is returned
-##  It exposes 3 methods
-## i.   get - It returns the supplied matrix
-## ii.  set - Sets the supplied matrix
-## ii.  setInverseMatrix - sets the InverseMatrix into the cache
-## iii. getInverseMatrix - returns the Inverse matrix from the cache
+## Function makeCacheMatrix- creates a special matrix for the input matrix and provides functions to access the special and input matrix
+## Assumption : Input matrix should be valid invertible matrix
+## @param x A Matrix 
+## @return list List of functions(set,get,setInverseMatrix,getInverseMatrix)
+## @examples
+## makeCacheMatrix(invertibleMatrix)
+
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
+  ## Sets the input matrix
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
+  ## Returns the input matrix
   get <- function() x
+  ## Sets the inverse matrix
   setInverseMatrix  <- function(inverseMatrix) m<<- inverseMatrix
+  ## Returns the inverse matrix
   getInverseMatrix <- function() m
   list(set=set, get=get, setInverseMatrix=setInverseMatrix,getInverseMatrix=getInverseMatrix)
 }
 
 
-## This function returns the inverse of the supplied special matrix of type cacehMatrix
+## This function cacheSolve returns the inverse of the supplied special matrix of type cacheMatrix
 ## First time it inverses the matrix using the "Solve" function and sets the same into cache.
 ## Subsequently returns the inverse from the cache 
+## @param inputSplMatrix A special matrix of type makeCacheMatrix
+## @return invX inverse of the input matrix
+## @examples
+## cacheSolve(cacheMatrix)
+
 cacheSolve <- function(inputSplMatrix, ...) {
         ## Return a matrix that is the inverse of 'x'
   invX <- inputSplMatrix$getInverseMatrix()
